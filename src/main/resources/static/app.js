c2e2cc0e86152380001ce52189bc4bea57254923
Nicolas Cardenas <nicocrash97@hotmail.com>
2019-10-06 20:29:08 -0500

    Parte 2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,7 +13,7 @@ var app = (function () {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+        ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
         ctx.stroke();
     };
     
@@ -21,6 +21,7 @@ var app = (function () {
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
+        stompClient.send("/topic/newpoint",{},JSON.stringify({x:evt.clientX-rect.left,y:evt.clientY-rect.top}));
         return {
             x: evt.clientX - rect.left,
             y: evt.clientY - rect.top
@@ -33,15 +34,15 @@ var app = (function () {
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
         
-        //subscribe to /topic/TOPICXX when connections succeed
+        //subscribe to /topic/newpoint when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
             stompClient.subscribe('/topic/newpoint', function (eventbody) {
-                alert(eventbody);
-                var eventJSONbody = JSON.parse(eventbody.body);
+                var extract = JSON.parse(eventbody.body);
+                var pnt = new Point(extract.x,extract.y);
+                addPointToCanvas(pnt);
             });
         });
-
     };
     
     
@@ -50,7 +51,9 @@ var app = (function () {
 
         init: function () {
             var can = document.getElementById("canvas");
-            
+            var canctxt = can.getContext('2d');
+            var res = can.addEventListener('mousedown',getMousePosition,false);
+
             //websocket connection
             connectAndSubscribe();
         },
